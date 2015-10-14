810653e8221525ef78da5b902c70972877f35fa6
itgsod-hampus-bohman <hampus.bohman@elev.it-gymnasiet.se>
2015-10-14 13:49:44 +0200

    fix
    fixed

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -2,17 +2,17 @@
 #function that asking the age of a person
 def ask_age()
   puts "Enter your age"
-  return gets.chomp.to_i
+  return gets.to_i
 end
 
 #
 def ticket_price(age)
   if (age < 18)
-    return "10 kr"
+    return 10
   elsif (age > 64)
-    return "15 kr"
+    return 15
   end
-  return "20 kr"
+  return 20
 end
 
 puts ticket_price(ask_age)
